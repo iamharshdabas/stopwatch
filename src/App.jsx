5dabf9fc2ac086f64a411c685ec9d06197507824
Harsh Dabas <iamharshdabas@gmail.com>
2023-05-21 16:59:37 +0530

    ADD: grid center

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Timer from './Timer'
 
 export default function App() {
   return (
-    <div className='bg-black text-gray-50 h-screen'>
+    <div className='bg-black text-gray-50 h-screen grid place-items-center'>
       <Timer duration={30} />
     </div>
   )
